9d9b68d14e9c1383d734f39ca431509c9b958f57
Ihimbru-k <kanihims25@gmail.com>
2023-12-05 09:23:24 +0100

    Implementing category items

---
+++ lib/category_item.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
