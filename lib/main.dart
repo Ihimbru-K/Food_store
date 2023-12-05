697c0ee4d4b824d0522a6aa4186420b2cff9dacb
Ihimbru-k <kanihims25@gmail.com>
2023-12-05 10:42:05 +0100

    completed adding of base theme to main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Foodstore',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         primarySwatch: Colors.pink,
-        accentColor : Colors.amber,
+       colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink).copyWith(secondary: Colors.amber),
+       canvasColor: Color.fromRGBO(255,254,229,1),
+        fontFamily: 'Raleway',
+      textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          bodyText2: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          subtitle1: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold,
+          )),
         useMaterial3: true,
       ),
       home: const Categories()
