b2edc6110ff10898ab90faccd5257f64ef02267d
Ihimbru-k <kanihims25@gmail.com>
2023-12-05 09:53:48 +0100

    added gridview to view food items in a scaffold

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -8,15 +8,18 @@ class Categories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(children:
-      
+    return Scaffold(
+      appBar: AppBar(title: Text("Kbfoods"),),
+      body: GridView(children:
+
       DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.title, catData.color)).toList(),
-      
-  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-      maxCrossAxisExtent: 200,
-      childAspectRatio: 3/2,
-      mainAxisExtent: 20,
-      crossAxisSpacing: 20,
-    ),);
+
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3/2,
+          mainAxisExtent: 20,
+          crossAxisSpacing: 20,
+        ),),
+    );
   }
 }
