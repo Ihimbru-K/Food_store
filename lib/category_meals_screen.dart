6ba067307619455a8a6e43da1488b0f97c172079
Ihimbru-k <kanihims25@gmail.com>
2023-12-07 11:56:06 +0100

    completed the usage of named routes to pass data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -9,8 +9,12 @@ class CategoryMealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final CategoryId = routeArgs['id'];
+    final CategoryTitle = routeArgs['title'];
+
     return Scaffold(
-      appBar: AppBar(title: Text(CategoryTitle),),
+      appBar: AppBar(title: Text(CategoryTitle!),),
 
       body: Center(child: Text("Category meals screen")),
     );
--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -6,7 +6,7 @@ const DUMMY_CATEGORIES = const{
   Category(id: '2', title: 'Fufu and khati-khati', color: Colors.green),
   Category(id: '3', title: 'Achu', color: Colors.blue),
   Category(id: '4', title: 'Ndole', color: Colors.red),
-  Category(id: '5', title: 'Ekwant', color: Colors.purple),
+  Category(id: '5', title: 'Ekwang', color: Colors.purple),
   Category(id: '6', title: 'Kwacoco', color: Colors.brown),
   Category(id: '7', title: 'Bongo-chobi', color: Colors.deepOrange),
 };
