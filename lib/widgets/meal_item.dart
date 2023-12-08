eeec708e36054bf7881334009ba5129af588576d
Ihimbru-k <kanihims25@gmail.com>
2023-12-08 16:57:25 +0100

    added feature to make meal title always readable

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:foodstore/model/meal.dart';
 
 class MealItem extends StatelessWidget {
-
   final String title;
   final String imageUrl;
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
 
-  MealItem({required this.title, required this.imageUrl, required this.duration, required this.complexity, required this.affordability});
+  MealItem(
+      {required this.title,
+      required this.imageUrl,
+      required this.duration,
+      required this.complexity,
+      required this.affordability});
 
-  void SelectMeal(){}
-
+  void SelectMeal() {}
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +23,40 @@ class MealItem extends StatelessWidget {
       onTap: SelectMeal,
       child: Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-      elevation: 4,
+        elevation: 4,
         margin: EdgeInsets.all(10),
         child: Column(
           children: <Widget>[
-            Stack(children: <Widget>[
-              ClipRRect(borderRadius: BorderRadius.only(topLeft:
-              Radius.circular(15), topRight: Radius.circular(15)),child: Image.network(imageUrl, height: 250, width: double.infinity, fit:BoxFit.cover ),),
-              Text(title, style: TextStyle(color: Colors.white, fontSize: 26, ), softWrap: true,overflow: TextOverflow.fade,)
-            ],)
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: Image.network(imageUrl,
+                      height: 250, width: double.infinity, fit: BoxFit.cover),
+                ),
+                Positioned(
+                  bottom: 20,
+                    right: 10,
+                    child: Container(
+                      width: 220,
+                      color: Colors.black54,
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                    ),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                )),
+              ],
+            )
           ],
         ),
       ),
-
-
     );
   }
 }
