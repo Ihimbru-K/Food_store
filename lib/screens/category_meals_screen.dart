9a9903b7fb75732bf6019c2dd8628246cbcbe8e8
Ihimbru-k <kanihims25@gmail.com>
2023-12-11 09:47:16 +0100

    Generating onGenerateRoute

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -19,7 +19,7 @@ class CategoryMealsScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(CategoryTitle!),),
-
+//s
       body: ListView.builder(itemBuilder: (ctx, idx) {
         return MealItem(title: CategoryMeals[idx].title, imageUrl: CategoryMeals[idx].title, duration: CategoryMeals[idx].duration, complexity: CategoryMeals[idx].complexity, affordability: CategoryMeals[idx].affordability, );}, itemCount: CategoryMeals.length)
     );
